e05e4817c04b7e4a8d74a8ed7e77683fe700f4a5
gigih santoso <gigih985042@gmail.com>
2024-05-20 22:09:46 +0700

    modified:   src/app.service.ts 	modified:   src/cek-api/cek-api.service.ts

--- src/cek-api/cek-api.service.ts
+++ src/cek-api/cek-api.service.ts
@@ -3,7 +3,14 @@ import { Request } from 'express';
 
 @Injectable()
 export class CekApiService {
-    getIpAddress(request: Request): string {
-        return request.ip;
+    getIpAddress(request: Request) {
+        let ip = request.headers['x-forwarded-for'] || request.socket.remoteAddress;
+        if (typeof ip === 'string') {
+          ip = ip.split(',')[0]; // Jika ada beberapa alamat IP, ambil yang pertama
+          if (ip.includes('::ffff:')) {
+            ip = ip.split('::ffff:')[1]; // Mengubah format IPv6 ke IPv4 jika perlu
+          }
+        }
+        return ip;
     }
 }
